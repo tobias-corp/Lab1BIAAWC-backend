c29314d79c8f327bdbb58ff4e0f02757e810a183
Tobi <75902541+Tobiasz-Hurny@users.noreply.github.com>
2023-03-05 19:56:11 +0100

    update app.py
    cleared code with pylint indications

--- app/app.py
+++ app/app.py
@@ -1,24 +1,33 @@
-from flask import Flask, send_from_directory
+"""
+A simple Flask app for greeting users and serving static files of frontend.
+"""
+
 import os
+from flask import Flask, send_from_directory
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
+    """
+    Renders a simple greeting to the user.
+    """
     return f'Hello, World!'
 
 
 @app.route('/greet/<name>')
 def greet(name):
+    """
+    Renders a personalized greeting to the user.
+    """
     return f'Hello, {name}!'
 
 
 @app.route('/static/<path:path>')
 def serve_static(path):
+    """
+    Serves a static frontend file from the 'static' directory.
+    """
     root_dir = os.getcwd()
     return send_from_directory(os.path.join(root_dir, 'static'), path)
-
-
-if __name__ == '__main__':
-    app.run()
